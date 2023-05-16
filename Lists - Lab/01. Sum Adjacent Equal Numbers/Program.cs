156982a1a765c2547b325ccce38fac0fc98035c6
Atanas-Semerdzhiev <131786587+Atanas-Semerdzhiev@users.noreply.github.com>
2023-05-16 15:38:07 +0300

    Fundamentalse C#
    New solutions Fundamentalse whit C#

---
+++ Lists - Lab/01. Sum Adjacent Equal Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Sum_Adjacent_Equal_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> inputArray = Console.ReadLine()
+                .Split(' ')
+                .Select(double.Parse)
+                .ToList();
+            for (int i = 0; i < inputArray.Count - 1; i++)
+            {
+
+
+                if (inputArray[i] == inputArray[i + 1])
+                {
+                    inputArray[i] = inputArray[i] + inputArray[i + 1];
+                    inputArray.RemoveAt(i + 1);
+
+
+                    i = -1;
+                }
+
+
+            }
+            Console.WriteLine(string.Join(" ", inputArray));
+
+        }
+    }
+}
---
+++ Lists - Lab/03. Merging Lists/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Merging_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> inputArray1 = Console.ReadLine()
+                  .Split(' ')
+                  .Select(int.Parse)
+                  .ToList();
+            List<int> inputArray2 = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            List<int> result = new List<int>();
+
+            int index = Math.Min(inputArray1.Count, inputArray2.Count);
+            for (int i = 0; i < index; i++)
+            {
+                result.Add(inputArray1[i]);
+                result.Add(inputArray2[i]);
+            }
+            if (inputArray1.Count > inputArray2.Count)
+            {
+                for (int i = index; i < inputArray1.Count; i++)
+                {
+                    result.Add(inputArray1[i]);
+                }
+            }
+            else if (inputArray2.Count > inputArray1.Count)
+            {
+                for (int i = index; i < inputArray2.Count; i++)
+                {
+                    result.Add(inputArray2[i]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", result.ToArray()));
+        }
+    }
+}
